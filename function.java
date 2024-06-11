1c21b4356d6c1961e5b0ccd67e69d086a56cc468
uttamgiri1316 <uttamgiri131689@gmail.com>
2024-06-11 15:18:38 +0530

    Add files via upload

---
+++ function.java
@@ -0,0 +1,12 @@
+package com.function;
+
+public class function {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//functiontest obj=new functiontest();
+		functiontest.add();	
+		functiontest.sub(34,64);
+	}
+
+}
---
+++ functiontest.java
@@ -0,0 +1,17 @@
+package com.function;
+
+public class functiontest {
+
+	public static void add() {
+		int a=30;
+		int b=2;
+		int total=a+b;
+		System.out.println("addition ="+total);
+		
+	}
+	public static void sub(int i,int j) {
+		
+		int total=i-j;
+		System.out.println("substraction ="+total);
+	}
+}
