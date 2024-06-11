d68a8f1a02f057bece925847a97d7e2d0fdf3eb5
uttamgiri1316 <uttamgiri131689@gmail.com>
2024-06-11 15:21:22 +0530

    Add files via upload

---
+++ even.java
@@ -0,0 +1,17 @@
+package com.statement;
+
+public class even {
+
+	public static void main(String[] args) {
+		System.out.println("Print even number form 10 to 1");
+		for(int i=10;i>=1;i--)
+		{
+			if(i%2==0)
+			{
+				System.out.println(i);
+			}
+		}
+
+	}
+
+}
