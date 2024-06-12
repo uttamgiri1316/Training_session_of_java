68178c12453a60889de5dbd9c5ae3565bd0fe3d5
uttamgiri1316 <uttamgiri131689@gmail.com>
2024-06-12 12:12:42 +0530

    Add files via upload

---
+++ InvertedHalfPymarid.java
@@ -0,0 +1,16 @@
+package com.selflearningdsa;
+
+public class InvertedHalfPymarid {
+
+	public static void main(String[] args) {
+		int n=4;
+		for(int i=n;i>=1;i--) {
+			for(int j=i;j>=1;j--) {
+			System.out.print("*");
+			}
+			System.out.println();
+		}
+	
+	}
+
+}
